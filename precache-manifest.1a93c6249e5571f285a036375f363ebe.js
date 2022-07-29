848bcd9c0970476b57af6e4e8c102a2a3fc826b1
codebool <codebool6700@gmail.com>
2022-07-29 00:30:54 -0400

    Deploy React app to GitHub Pages

---
+++ precache-manifest.1a93c6249e5571f285a036375f363ebe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "cf727512c596f3f03520",
+    "url": "/welbi/static/css/main.ad511d00.chunk.css"
+  },
+  {
+    "revision": "cf727512c596f3f03520",
+    "url": "/welbi/static/js/main.27c04c1c.chunk.js"
+  },
+  {
+    "revision": "d08390e0f135bca1eaf5",
+    "url": "/welbi/static/js/runtime~main.56c77b43.js"
+  },
+  {
+    "revision": "316cc844a04bf0ef5f9d",
+    "url": "/welbi/static/js/2.926424c2.chunk.js"
+  },
+  {
+    "revision": "a03a2b79c7dfa4b66d3d",
+    "url": "/welbi/static/js/3.7fa365a8.chunk.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/welbi/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "6a10af71e231bfe2ff579f0a3cf08305",
+    "url": "/welbi/index.html"
+  }
+];
