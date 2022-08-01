22c1a79399c0e1c395f51eeef862b313d5ef1db4
codebool <codebool6700@gmail.com>
2022-08-01 05:37:19 -0400

    Updates

---
+++ precache-manifest.a775384b5243318ca563f1593a62f525.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "eaddb95849032c437a0f",
+    "url": "/static/css/main.5c03feef.chunk.css"
+  },
+  {
+    "revision": "eaddb95849032c437a0f",
+    "url": "/static/js/main.d9ca152e.chunk.js"
+  },
+  {
+    "revision": "b29d8a849139f64ab4ee",
+    "url": "/static/js/runtime~main.c58ff8e6.js"
+  },
+  {
+    "revision": "7d93bb67f635a7543a1a",
+    "url": "/static/js/2.1d7c7fd7.chunk.js"
+  },
+  {
+    "revision": "408621d3a15eaf977c3c",
+    "url": "/static/js/3.87f4fc67.chunk.js"
+  },
+  {
+    "revision": "24d7538ab7c25fdbda19d44294b9bf7b",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/welbi/precache-manifest.4a906a7aba4a81ac1261886e02b01170.js"
+  "/precache-manifest.a775384b5243318ca563f1593a62f525.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/welbi/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
