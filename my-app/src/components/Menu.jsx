3409afc1a18bc4888c5542056e045331724e46ad
codebool <codebool6700@gmail.com>
2022-08-09 23:32:38 -0400

    clean up code

--- my-app/src/components/Menu.jsx
+++ my-app/src/components/Menu.jsx
@@ -5,17 +5,11 @@ export default class Menu extends Component {
     render() {
         return <div className='wrapper text-center'>
             <Link to={'/resident'} className={'box'}>
-                {/* <div className='card-icon'>
-                    <img src="..//assets/images/logo512.png" alt="no resident pic" className="card-icon"/>
-                </div> */}
                 <h3>Residents</h3>
                 <p className='card-description'>This is where you can view and edit your residents.</p>
             </Link>
 
             <Link to={'/program'} className={'box'}>
-                {/* <div className='card-icon'>
-                    <img src="../assets/images/Family-Silhouette.svg" alt="no program pic" className="card-icon"/>
-                </div> */}
                 <h3>Programs</h3>
                 <p className='card-description'>This is where you can view and edit your programs.</p>
             </Link>
